3c8956d46189d2e6655eb5deee08902345a35eda
Danielle Souza <dany.sa1@hotmail.com>
2025-05-15 12:23:32 -0300

    refactor: adjust text

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -33,7 +33,7 @@ const Table = ({ posts }) => {
   );
 
   return (
-    <>    
+    <>
       <div className="py-4 m-5 mb-0 bg-white">
         <div className="relative mt-1 bg-white">
           <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -63,115 +63,115 @@ const Table = ({ posts }) => {
           />
         </div>
       </div>
-    <div className="relative overflow-x-auto shadow-md rounded-lg bg-amber-700 m-5 mt-0">
-      <form
-        id="inline-form"
-        onSubmit={(event) => {
-          event.preventDefault();
-          handleSave();
-        }}
-      ></form>
-      <table
-        className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-        id="search-table"
-      >
-        <thead className="text-xs  rounded-lg  text-gray-700 uppercase dark:bg-sky-600 dark:text-sky-50">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Id
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Título
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Texto
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Usuário
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Editar
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredPost.map((item) =>
-            currentPost?.id === item.id ? (
-              <React.Fragment key={item.id}>
+      <div className="relative overflow-x-auto shadow-md rounded-lg bg-amber-700 m-5 mt-0">
+        <form
+          id="inline-form"
+          onSubmit={(event) => {
+            event.preventDefault();
+            handleSave();
+          }}
+        ></form>
+        <table
+          className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+          id="search-table"
+        >
+          <thead className="text-xs  rounded-lg  text-gray-700 uppercase dark:bg-sky-600 dark:text-sky-50">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Id
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Título
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Texto
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Usuário
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Ações
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredPost.map((item) =>
+              currentPost?.id === item.id ? (
+                <React.Fragment key={item.id}>
+                  <tr
+                    key={item.id}
+                    className="bg-white dark:bg-blue-100 border-gray-200 dark:text-blue-600 dark:focus:bg-sky-50 "
+                  >
+                    <th
+                      scope="row"
+                      className="px-6 py-4  bg-sky-50 font-medium text-gray-900 whitespace-nowrap dark:text-blue-600"
+                    >
+                      {item.id}
+                    </th>
+                    <td className="px-6 py-4 bg-sky-50">
+                      <input
+                        required
+                        maxLength={50}
+                        className="px-6 py-4 w-full border rounded-lg"
+                        name="title"
+                        form="inline-form"
+                        value={currentPost.title}
+                        onChange={(e) => {
+                          handleChange(e);
+                        }}
+                      />
+                    </td>
+                    <td className="px-6 py-4  bg-sky-50">
+                      <input
+                        maxLength={200}
+                        className="px-6 py-4 w-full border rounded-lg"
+                        required
+                        name="body"
+                        form="inline-form"
+                        value={currentPost.body}
+                        onChange={(e) => {
+                          handleChange(e);
+                        }}
+                      />
+                    </td>
+                    <td className="px-6 py-4  bg-sky-50">{item.user}</td>
+                    <td className="px-6 py-4  bg-sky-50">
+                      <button form="inline-form">
+                        <SaveIcon />
+                      </button>
+                    </td>
+                  </tr>
+                </React.Fragment>
+              ) : (
                 <tr
                   key={item.id}
-                  className="bg-white dark:bg-blue-100 border-gray-200 dark:text-blue-600 dark:focus:bg-sky-50 "
+                  className="border-b border-gray-200  odd:dark:bg-sky-50  even:dark:bg-sky-200 dark:text-sky-950  focus:bg-sky-50"
                 >
                   <th
                     scope="row"
-                    className="px-6 py-4  bg-sky-50 font-medium text-gray-900 whitespace-nowrap dark:text-blue-600"
+                    className="px-6 py-4 font-medium  whitespace-nowrap"
                   >
                     {item.id}
                   </th>
-                  <td className="px-6 py-4 bg-sky-50">
-                    <input
-                      required
-                      maxLength={50}
-                      className="px-6 py-4 w-full border rounded-lg"
-                      name="title"
-                      form="inline-form"
-                      value={currentPost.title}
-                      onChange={(e) => {
-                        handleChange(e);
-                      }}
-                    />
-                  </td>
-                  <td className="px-6 py-4  bg-sky-50">
-                    <input
-                      maxLength={200}
-                      className="px-6 py-4 w-full border rounded-lg"
-                      required
-                      name="body"
-                      form="inline-form"
-                      value={currentPost.body}
-                      onChange={(e) => {
-                        handleChange(e);
-                      }}
-                    />
-                  </td>
-                  <td className="px-6 py-4  bg-sky-50">{item.user}</td>
-                  <td className="px-6 py-4  bg-sky-50">
-                    <button form="inline-form">
-                      <SaveIcon />
-                    </button>
+                  <td className="px-6 py-4 ">{item.title}</td>
+                  <td className="px-6 py-4">{item.body}</td>
+                  <td className="px-6 py-4">{item.user}</td>
+                  <td className="px-6 py-4">
+                    <EditIcon onClick={() => setCurrentPost(item)} />
                   </td>
                 </tr>
-              </React.Fragment>
-            ) : (
-              <tr
-                key={item.id}
-                className="border-b border-gray-200  odd:dark:bg-sky-50  even:dark:bg-sky-200 dark:text-sky-950  focus:bg-sky-50"
-              >
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium  whitespace-nowrap"
-                >
-                  {item.id}
-                </th>
-                <td className="px-6 py-4 ">{item.title}</td>
-                <td className="px-6 py-4">{item.body}</td>
-                <td className="px-6 py-4">{item.user}</td>
-                <td className="px-6 py-4">
-                  <EditIcon onClick={() => setCurrentPost(item)} />
+              )
+            )}
+            {filteredPost.length === 0 && (
+              <tr className="bg-white border-b dark:bg-blue-100 dark:border-b-blue-800 border-gray-200 dark:text-blue-600 ">
+                <td colSpan={5} className="px-6 py-4 text-center ">
+                  Não foi encontrado nenhum resultado para essa pesquisa
                 </td>
               </tr>
-            )
-          )}
-          {filteredPost.length === 0 && (
-            <tr className="bg-white border-b dark:bg-blue-100 dark:border-b-blue-800 border-gray-200 dark:text-blue-600 ">
-              <td colSpan={5} className="px-6 py-4 text-center ">
-                Não foi encontrado nenhum resultado para essa pesquisa
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
+            )}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
