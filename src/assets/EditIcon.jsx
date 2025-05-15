aa20b41e9ecd20ae2af818b18e9446195416e295
Danielle Souza <dany.sa1@hotmail.com>
2025-05-15 13:19:50 -0300

    refactor: add title for icons

--- src/assets/EditIcon.jsx
+++ src/assets/EditIcon.jsx
@@ -4,6 +4,7 @@ const EditIcon = ({ className = "", ...props }) => (
   <span
     className={`inline-flex items-center justify-center text-gray-600 hover:text-sky-600 transition-colors cursor-pointer ${className}`}
     {...props}
+    title="Editar"
   >
     <svg
       xmlns="http://www.w3.org/2000/svg"
--- src/assets/SaveIcon.jsx
+++ src/assets/SaveIcon.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const SaveIcon = ({ className = "w-5 h-5 text-blue-600 cursor-pointer", onClick }) => {
   return (
+    <span title="Salvar">
     <svg
       onClick={onClick}
       className={className}
@@ -11,6 +12,7 @@ const SaveIcon = ({ className = "w-5 h-5 text-blue-600 cursor-pointer", onClick 
     >
       <path d="M17 3a1 1 0 0 1 .707.293l.007.007a1 1 0 0 1 .286.707V16a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4c0-.552.448-1 1-1h13zm-2 6H5v7h10V9zM6 10h2v2H6v-2zM14 4H5v3h9V4z" />
     </svg>
+    </span>
   );
 };
 
